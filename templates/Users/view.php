e0c84bcf38515976207df6cda76be8e5883993c6
ethan <yjia0139@monash.student.edu>
2023-08-15 05:03:09 +1000

    user management has been partially done

--- templates/Users/view.php
+++ templates/Users/view.php
@@ -4,38 +4,29 @@
  * @var \App\Model\Entity\User $user
  */
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Edit User'), ['action' => 'edit', $user->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete User'), ['action' => 'delete', $user->id], ['confirm' => __('Are you sure you want to delete # {0}?', $user->id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List Users'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('New User'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="users view content">
-            <h3><?= h($user->id) ?></h3>
-            <table>
-                <tr>
-                    <th><?= __('Access Role') ?></th>
-                    <td><?= h($user->access_role) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Email') ?></th>
-                    <td><?= h($user->email) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($user->id) ?></td>
-                </tr>
-            </table>
-            <div class="text">
-                <strong><?= __('Username') ?></strong>
-                <blockquote>
-                    <?= $this->Text->autoParagraph(h($user->username)); ?>
-                </blockquote>
+<div class="container my-5">
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <div class="card">
+                <div class="card-header bg-primary text-white">
+                    <h3 class="card-title mb-0"><?= h($user->username) ?></h3>
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <tr>
+                            <th><?= __('Username') ?></th>
+                            <td><?= h($user->username) ?></td>
+                        </tr>
+                        <tr>
+                            <th><?= __('Email') ?></th>
+                            <td><?= h($user->email) ?></td>
+                        </tr>
+                        <tr>
+                            <th><?= __('Access Role') ?></th>
+                            <td><?= h($user->access_role) ?></td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
