565eb0a68a135006fd61d7dcebc7a0b10c84709f
nrod0008 <nrod0008@student.monash.edu>
2023-08-25 11:59:39 +1000

    Fixed alot of bugs found in many pages.

--- templates/Moncases/userlist.php
+++ templates/Moncases/userlist.php
@@ -32,10 +32,10 @@ $this->disableAutoLayout();
     <?= $this->Html->charset() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimal-ui">
     <title>Case List</title>
+
     <?= $this->Html->meta('icon', 'favicon.ico', ['type' => 'image/x-icon']) ?>
     <?= $this->Html->css('/assets/css/master.css') ?>
-    <?= $this->Html->css('/assets/plugins/iview/css/iview.css') ?>
-    <?= $this->Html->css('/assets/plugins/iview/css/skin/style.css', ['class' => 'skin']) ?>
+
 
     <style>
         .section-bg_second, .jelect-option_state_active, .jelect-option:hover, .btn-skew-r{
@@ -58,6 +58,9 @@ $this->disableAutoLayout();
 
         a, .color_primary, .ui-title-inner .icon:before, .link-img__link:hover .link-img__title, .main-block__title strong, .decor-3, .list-services:hover .list-services__title, .list-progress .icon, .footer-title__inner, .card__price-number, .list-categories__link:before, .list-categories__link:hover, .list-descriptions dt:before, .widget-post1__price, .nav-tabs > li.active > a, .nav-tabs > li > a:hover, .nav-tabs > li.active > a:focus, .social-blog__item:before, blockquote:before, .comments-list .comment-datetime {
             color: #7d9afd;
+        }
+        .panel-group .panel {
+            padding-left: 0px;
         }
 
     </style>
@@ -196,65 +199,82 @@ $this->disableAutoLayout();
                     <aside class="sidebar">
                         <!-- FILTER CASE TYPE-->
                         <?= $this->Form->create(null, ['url' => ['controller' => 'moncases', 'action' => 'userlist'], 'type' => 'get']) ?>
-                        <section class="widget widget_mod-a">
-                            <h3 class="widget-title">CASE TYPE</h3>
-                            <div class="decor-1"></div>
-                            <div class="widget-content">
-                                <?= $this->Form->select('case_type', [
-                                    'Oscer' => 'Oscer',
-                                    'Long' => 'Long',
-                                    'Medium' => 'Medium',
-                                    'Short' => 'Short',
-                                    'General' => 'General',
-                                ], [
-                                    'class' => 'select select_mod-a jelect',
-                                    'empty' => 'Choose Case Type',
-                                ]); ?>
-                            </div>
-                        </section>
-                        <section class="widget widget_mod-a">
-                            <h3 class="widget-title">Contributer</h3>
-                            <div class="decor-1"></div>
-                            <div class="widget-content">
-                                <?= $this->Form->select('contributor', [
-                                    'Trainee' => 'Trainee',
-                                    'Consultant' => 'Consultant',
-                                    'Library' => 'Library',
-                                ], [
-                                    'class' => 'select select_mod-a jelect',
-                                    'empty' => 'Choose Contributor',
-                                ]); ?>
-                            </div>
-                        </section>
-                        <section class="widget widget_mod-a">
-                            <h3 class="widget-title">Rating</h3>
-                            <div class="decor-1"></div>
-                            <div class="widget-content">
-                                <?= $this->Form->select('rating', [
-                                    '1' => '1',
-                                    '2' => '2',
-                                    '3' => '3',
-                                    '4' => '4',
-                                    '5' => '5',
-                                ], [
-                                    'class' => 'select select_mod-a jelect',
-                                    'empty' => 'Choose Rating',
-                                ]); ?>
-                            </div>
-                        </section>
-                        <!-- <section class="widget widget_mod-a">
-                                <h3 class="widget-title">Imaging</h3>
-                                <div class="decor-1"></div>
-                                <div class="widget-content">
-                                    <?= $this->Form->select('Imaging', [
-                            'Test' => 'Test',
-                        ], [
-                            'class' => 'select select_mod-a jelect',
-                            'empty' => 'Choose Imaging',
-                        ]); ?>
+                        <div class="panel-group accordion" id="accordion-1">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <a class="btn-collapse" data-toggle="collapse" data-parent="#accordion-1" href="#collapse-1"><i class="icon"></i></a>
+                                    <h3 class="panel-title">Case Type</h3>
                                 </div>
-                            </section>
-                            -->
+                                <div id="collapse-1" class="panel-collapse collapse in">
+                                    <div class="panel-body">
+                                        <?= $this->Form->select('case_type', [
+                                            'Oscer' => 'Oscer',
+                                            'Long' => 'Long',
+                                            'Medium' => 'Medium',
+                                            'Short' => 'Short',
+                                            'General' => 'General',
+                                        ], [
+                                            'class' => 'select select_mod-a jelect',
+                                            'empty' => 'Choose Case Type',
+                                        ]); ?>
+                                    </div>
+                                </div>
+                            </div><!-- end panel -->
+
+                        <div class="panel-group accordion" id="accordion-2">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <a class="btn-collapse" data-toggle="collapse" data-parent="#accordion-2" href="#collapse-2"><i class="icon"></i></a>
+                                    <h3 class="panel-title">Contributor</h3>
+                                </div>
+                                <div id="collapse-2" class="panel-collapse collapse in">
+                                    <div class="panel-body">
+                                        <?= $this->Form->select('contributor', [
+                                            'Trainee' => 'Trainee',
+                                            'Consultant' => 'Consultant',
+                                            'Library' => 'Library',
+                                        ], [
+                                            'class' => 'select select_mod-a jelect',
+                                            'empty' => 'Choose Contributor',
+                                        ]); ?>
+                                    </div>
+                                </div>
+                            </div><!-- end panel -->
+
+                            <div class="panel-group accordion" id="accordion-3">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <a class="btn-collapse" data-toggle="collapse" data-parent="#accordion-3" href="#collapse-3"><i class="icon"></i></a>
+                                    <h3 class="panel-title">Rating</h3>
+                                </div>
+                                <div id="collapse-3" class="panel-collapse collapse in">
+                                    <div class="panel-body">
+                                        <?= $this->Form->select('rating', [
+                                            '1' => '1',
+                                            '2' => '2',
+                                            '3' => '3',
+                                            '4' => '4',
+                                            '5' => '5',
+                                        ], [
+                                            'class' => 'select select_mod-a jelect',
+                                            'empty' => 'Choose Rating',
+                                        ]); ?>                                    </div>
+                                </div>
+                            </div><!-- end panel -->
+
+                            <!-- <section class="widget widget_mod-a">
+                                    <h3 class="widget-title">Imaging</h3>
+                                    <div class="decor-1"></div>
+                                    <div class="widget-content">
+                                        <?= $this->Form->select('Imaging', [
+                                'Test' => 'Test',
+                            ], [
+                                'class' => 'select select_mod-a jelect',
+                                'empty' => 'Choose Imaging',
+                            ]); ?>
+                                    </div>
+                                </section>
+                                -->
                         <div class="widget-content">
                             <div class="btn">
                                 <div class="btn-filter btn-skew-r js-filter" style ="padding: 0px 10px 0px;">
