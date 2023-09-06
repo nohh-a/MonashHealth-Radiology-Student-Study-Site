ffa5e9f23d5e3072e6db82b2c94fcf408e1b1e3b
ethan <yjia0139@monash.student.edu>
2023-09-06 17:44:18 +1000

    the table is hard to be suit for the mobile side, may recommend the Admin to use PC or laptop to manage the users

--- templates/Users/index.php
+++ templates/Users/index.php
@@ -34,6 +34,15 @@ echo $this->Html->script('/vendor/datatables/dataTables.bootstrap4.min.js', ['bl
         white-space: nowrap;
     }
 
+    /* mobile */
+    @media (max-width: 768px) {
+
+        table th, table td {
+            font-size: 14px;
+
+        }
+    }
+
     .container-fluid {
         padding-left: 0px;
         padding-right: 0px;
@@ -43,10 +52,12 @@ echo $this->Html->script('/vendor/datatables/dataTables.bootstrap4.min.js', ['bl
 </style>
 
 <div class="container">
-    <div class="row align-items-center d-flex justify-content-center" data-animate="fadeInUp">
-        <div class="heading-text heading-section text-center mt-5">
-            <h1 style="color: black; font-weight: bold;">Users</h1>
-            <p> </p>
+    <div class="row align-items-center justify-content-center" data-animate="fadeInUp">
+        <div class="col-md-12">
+            <div class="heading-text heading-section text-center mt-5">
+                <h1 style="color: black; font-weight: bold;">Users</h1>
+                <p> </p>
+            </div>
         </div>
     </div>
 
@@ -67,7 +78,6 @@ echo $this->Html->script('/vendor/datatables/dataTables.bootstrap4.min.js', ['bl
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                     <tr>
-
                         <th><?= h('Username') ?></th>
                         <th><?= h('First Name') ?></th>
                         <th><?= h('Last Name') ?></th>
@@ -75,29 +85,24 @@ echo $this->Html->script('/vendor/datatables/dataTables.bootstrap4.min.js', ['bl
                         <th><?= h('Role') ?></th>
                         <th><?= h('Contributor') ?></th>
                         <th><?= h('Action') ?></th>
-
                     </tr>
                     </thead>
                     <tbody>
                     <?php foreach ($users as $user) : ?>
                         <tr>
-
                             <td><?= h($user->username) ?></td>
                             <td><?= h($user->first_name) ?></td>
                             <td><?= h($user->last_name) ?></td>
                             <td><?= h($user->email) ?></td>
                             <td><?= h($user->access_role) ?></td>
                             <td><?= h($user->contributor) ?></td>
-
-
-
                             <td class="actions">
                                 <?= $this->Html->link(__('Edit'), ['action' => 'edit', $user->id], ['class' => 'btn btn-info']) ?>
                                 <?= $this->Html->link(__('Change Password'), ['controller' => 'Auth', 'action' => 'change-password', $user->id], ['class' => 'btn btn-warning']) ?>
                                 <?php if ($user->access_role !== 'ADMIN') : ?>
-                                <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $user->id], [
-                                    'class' => 'btn btn-danger',
-                                    'confirm' => __("Are you sure you want to delete this user?\n{0} {1} ({2})", $user->first_name, $user->last_name, $user->email)]) ?>
+                                    <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $user->id], [
+                                        'class' => 'btn btn-danger',
+                                        'confirm' => __("Are you sure you want to delete this user?\n{0} {1} ({2})", $user->first_name, $user->last_name, $user->email)]) ?>
                                 <?php endif; ?>
                             </td>
                         </tr>
@@ -105,18 +110,16 @@ echo $this->Html->script('/vendor/datatables/dataTables.bootstrap4.min.js', ['bl
                     </tbody>
                 </table>
             </div>
-
-            <script>
-                $(document).ready(function() {
-                    $('#dataTable').DataTable();
-                });
-
-            </script>
         </div>
     </div>
-
-    <br>
-
 </div>
+
+<script>
+    $(document).ready(function() {
+        $('#dataTable').DataTable();
+    });
+</script>
+
+
 
 
