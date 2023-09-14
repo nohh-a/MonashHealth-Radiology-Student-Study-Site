a4e8a5133cfde34c9e4b54a137a6e9b1b87a66c3
ethan <yjia0139@monash.student.edu>
2023-09-15 01:46:30 +1000

    change the layout of select case type

--- templates/Moncases/addnewcase.php
+++ templates/Moncases/addnewcase.php
@@ -21,53 +21,52 @@
                 <div class="col-md-6">
                     <a class="nav-button active" href="<?= $this->Url->build(['controller'=>'Moncases','action'=> 'userlist']) ?>"> <?= $this->Html->link(__('Home'), ['action' => 'userlist'], ['class' => 'btn btn-primary']) ?></a>
                     <br><br>
-                    <div class = "card">
+
                         <h5 class="card-header text-center"><?= __('Select Case Type') ?></h5>
-                        <div class ="card-body row justify-content-center align-items-center">
-                            <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" style= "height: 50px;">
-                                <!--<option selected>Choose a case</option>-->
-                                <option value="1">Oscer</option>
-                                <option value="2">Long Case</option>
-                                <option value="3">Medium Case</option>
-                                <option value="4">Short Case</option>
-                                <option value="5">General Case</option>
-                            </select>
-                        </div>
-                    </div>
+
                     <br><br>
-                    <?= $this->Html->link(__('Back'), ['action' => 'userlist'], ['class' => 'btn btn-secondary']) ?>
-                    <button type="button" class="btn btn-primary float-right" onClick = "selectionpick()">Next</button>
+
+                        <div class ="card-body row justify-content-center align-items-center">
+                            <div class="col-lg-6">
+                                <div class="card mb-2 py-1 border-bottom-primary">
+                                    <div class="card-body">
+                                        <a href="<?= $this->Url->build(['controller' => 'Moncases', 'action' => 'addoscer']) ?>" class="btn btn-block">Oscer Case</a>
+                                    </div>
+                                </div>
+
+                                <div class="card mb-2 py-1 border-bottom-secondary">
+                                    <div class="card-body">
+                                        <a href="<?= $this->Url->build(['controller' => 'Moncases', 'action' => 'addlong']) ?>" class="btn btn-block">Long Case</a>
+                                    </div>
+                                </div>
+
+                                <div class="card mb-2 py-1 border-bottom-success">
+                                    <div class="card-body">
+                                        <a href="<?= $this->Url->build(['controller' => 'Moncases', 'action' => 'addmedium']) ?>" class="btn btn-block">Medium Case</a>
+                                    </div>
+                                </div>
+
+                                <div class="card mb-2 py-1 border-bottom-info">
+                                    <div class="card-body">
+                                        <a href="<?= $this->Url->build(['controller' => 'Moncases', 'action' => 'addshort']) ?>" class="btn btn-block">Short Case</a>
+                                    </div>
+                                </div>
+
+                                <div class="card mb-2 py-1 border-bottom-warning">
+                                    <div class="card-body">
+                                        <a href="<?= $this->Url->build(['controller' => 'Moncases', 'action' => 'addgeneral']) ?>" class="btn btn-block">General Case</a>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
                 </div>
             </div>
         </div>
     </div>
-
 </div>
-<script type="text/javascript">
-    function selectionpick(){
-        var selectElement = document.querySelector('.form-select'); // select the select element
-        var selectedValue = selectElement.value; // get the value of the selected option
 
-        if (selectedValue == "1"){
-            window.location.href =  "<?= $this->Url->build(['controller' => 'Moncases', 'action' => 'addoscer']) ?>";
-        }
-        if (selectedValue == "2"){
-            window.location.href =  "<?= $this->Url->build(['controller' => 'Moncases', 'action' => 'addlong']) ?>";
-        }
-        if (selectedValue == "3"){
-            window.location.href =  "<?= $this->Url->build(['controller' => 'Moncases', 'action' => 'addmedium']) ?>";
-        }
-        if (selectedValue == "4"){
-            window.location.href =  "<?= $this->Url->build(['controller' => 'Moncases', 'action' => 'addshort']) ?>";
-        }
-        if (selectedValue == "5"){
-            window.location.href =  "<?= $this->Url->build(['controller' => 'Moncases', 'action' => 'addgeneral']) ?>";
-        }
 
-    }
-</script>
 
-<?= $this->Html->script(
-    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js',
-    ['integrity' => 'sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm', 'crossorigin' => 'anonymous']
-) ?>
+
