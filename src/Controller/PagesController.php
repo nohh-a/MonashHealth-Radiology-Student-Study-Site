f7680872847ff5e633fc516f931547cbdb60d30f
ZHAOHUI_WANG <zwan0237@student.monash.edu>
2023-10-27 15:56:10 +1100

    drop last commit

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -33,27 +33,6 @@ class PagesController extends AppController
 {
 
     /**
-     * Controller initialize override
-     *
-     * @return void
-     */
-    public function initialize(): void {
-        parent::initialize();
-
-        // Controller-level function/action whitelist for authentication
-        $this->Authentication->allowUnauthenticated(['home']);
-    }
-
-    /**
-     * Home page action
-     *
-     * @return \Cake\Http\Response|null|void Renders view
-     */
-    public function home() {
-
-    }
-
-    /**
      * Displays a view
      *
      * @param string ...$path Path segments.
